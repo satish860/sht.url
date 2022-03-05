79bd5a65d7f23475cd167bdb6e90329b6274d7d6
satish <satish860@gmail.com>
2022-03-05 18:33:36 +0530

    Added the Create Url with the Slug Generator

--- src/Sht.url/Sht.url.Api/Api/CreateUrl/Endpoint.cs
+++ src/Sht.url/Sht.url.Api/Api/CreateUrl/Endpoint.cs
@@ -12,10 +12,12 @@ namespace Sht.url.Api.Api.CreateUrl
 
         public override async Task HandleAsync(CreateShortUrl req, CancellationToken ct)
         {
+            var slug = await Nanoid.Nanoid.GenerateAsync("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", 12);
             await SendAsync(new CreateShortUrlResponse
             {
+                ShortUrl = $"{_httpContext.Request.Scheme}://{_httpContext.Request.Host}/{slug}",
                 Url = req.Url,
-            });
+            }) ;
         }
     }
 }
